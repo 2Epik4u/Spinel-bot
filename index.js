8bbf24806dab578ff30b6ec430dab77e739b401b
2Epik4u <justinabosssmlg@gmail.com>
2024-01-15 23:34:22 -0500

    index.js 10/3/2019

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("message", async message => {
-  const prefix = ":";
-
-  if (!Message.author.bot) return;
-  if (message.guild) return;
-  if (message.conent.startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd === "ping") {
-    const msg = await message.channel.send('🏓 Pinging....');
-
-    msg.edit(`🏓 Pong\nLatency is $(Math.floor(msg.Createdalt - message.Createdalt\nAPI latency $(Math.round(client.ping)]ms`);
-  } 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-if (cmd === "whatisstevenuniverse") {
-  const msg = await message.channel.send('Steven universe is a show on cartoon network. The show came out in May 21, 2013, There is also a movie of it called Steven universe the movie. and you can also you could watch steven universe the movie on that website!')
+client.on('message', msg => {
+  if (msg.content === 'help') {
+    msg.reply('I am Spinels bot, she made me if people dont know about something about steven universe you can ask spinel! Here are a list of commands :stevengem steven gem shows you what stevens gem is! :whodiamondleader tells you who the diamond leader is. If you dont know about steven universe is type :whatisstevenuinverse in the chat!')
+  } else if (msg.content === ':stevengem' ) {
+    msg.reply('Stevens gem is a Pink diamond gem, This is confirmed in Change your mind in season 5 Episode 29-32 when white diamond took stevens gem')
+} else if (msg.content === ':whodiamondleader' ) {
+    msg.reply('The leader of the Diamond Authority is White Diamond because she is every color of the light')
+ } else if (msg.content === ':whatisstevenuinverse' ) {
+    msg.reply('Steven universe is a show on cartoon network. The show came out in May 21, 2013, There is also a movie of it called Steven universe the movie. If you want too watch steven universe but dont want to watch it on your tv go to https://worldofsteven.com and you can also you could watch steven universe the movie on that website!')
+  } else if (msg.content === 'steven universe sucks' ) {
+     msg.reply('https://tenor.com/view/steven-universe-steven-universe-movie-spinel-angry-cartoon-network-gif-14955865')
+   } else if (msg.content === 'SU sucks' ) {
+      msg.reply('https://tenor.com/view/steven-universe-steven-universe-movie-spinel-angry-cartoon-network-gif-14955865')
+    } else if (msg.content === 'Steven Universe sucks' ) {
+       msg.reply('https://tenor.com/view/steven-universe-steven-universe-movie-spinel-angry-cartoon-network-gif-14955865')
+     } else if (msg.content === 'Spinel is awesome' ) {
+        msg.reply('https://tenor.com/view/spinel-steven-universe-dance-steven-universe-the-movie-gif-14974036')
+      } else if (msg.content === 'Spinel is awesome!' ) {
+         msg.reply('https://tenor.com/view/spinel-steven-universe-dance-steven-universe-the-movie-gif-14974036')
+       } else if (msg.content === 'Spinel is the best!' ) {
+          msg.reply('https://tenor.com/view/spinel-steven-universe-dance-steven-universe-the-movie-gif-14974036')
+        } else if (msg.content === 'Spinel is the best' ) {
+           msg.reply('https://tenor.com/view/spinel-steven-universe-dance-steven-universe-the-movie-gif-14974036')
+         } else if (msg.content === 'Spinel is the worst!' ) {
+            msg.reply('https://tenor.com/view/spinel-steven-universe-crying-sad-gif-15032493')
+          } else if (msg.content === 'Spinel is the worst Steven Universe character!' ) {
+             msg.reply('https://tenor.com/view/spinel-steven-universe-crying-sad-gif-15032493')
+           } else if (msg.content === 'Spinel is the worst Steven Universe character' ) {
+              msg.reply('https://tenor.com/view/spinel-steven-universe-crying-sad-gif-15032493')
+            } else if (msg.content === 'Spinels song sucks' ) {
+               msg.reply('https://tenor.com/view/spinel-steven-universe-crying-sad-gif-15032493')
+             } else if (msg.content === 'Spinel is shit' ) {
+                msg.reply('https://tenor.com/view/spinel-steven-universe-crying-sad-gif-15032493')
 
 
-client.login('NjI4NjA3MTkyODA2OTgxNjMy.XswewQ.q7RWAXiBS4Tel12EWuJjsXs6qzM')
+
+}
+});
+
+client.login('NjI4NjA3MTkyODA2OTgxNjMy.XZNyCw.cNjUuecSjTtPdMUK76nm1bVLqBc');
